1befe5c7df522aa9afbe5c6f58e3522fea1389c4
ahmet.ozel <ahmet.ozel@tubitak.gov.tr>
2024-09-24 00:26:05 +0300

    tests are removed

--- src/test/java/com/github/ozelahmet/antipatterns/codepollution/second/FakeLogger.java
+++
@@ -1,9 +0,0 @@
-package com.github.ozelahmet.antipatterns.codepollution.second;
-
-public class FakeLogger implements Logger {
-
-    @Override
-    public void info(String message) {
-        // do nothing
-    }
-}
